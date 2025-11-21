f0f8f7199fae0fee15c8e484a483288580a879da
Abhijit Mohanty <abhijitmohanty334@gmail.com>
2025-11-22 02:38:13 +0530

    Create 19.java

---
+++ 19.java
@@ -0,0 +1,64 @@
+class Buffer {
+    private int data;
+    private boolean available = false;
+
+    public synchronized void produce(int value) throws InterruptedException {
+        while (available) {
+            wait();
+        }
+        data = value;
+        available = true;
+        System.out.println("Produced: " + data);
+        notify();
+    }
+
+    public synchronized int consume() throws InterruptedException {
+        while (!available) {
+            wait();
+        }
+        available = false;
+        System.out.println("Consumed: " + data);
+        notify();
+        return data;
+    }
+}
+
+class Producer extends Thread {
+    private Buffer buffer;
+
+    public Producer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                buffer.produce(i);
+            } catch (Exception e) {}
+        }
+    }
+}
+
+class Consumer extends Thread {
+    private Buffer buffer;
+
+    public Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                buffer.consume();
+            } catch (Exception e) {}
+        }
+    }
+}
+
+public class ProducerConsumer {
+    public static void main(String[] args) {
+        Buffer b = new Buffer();
+        new Producer(b).start();
+        new Consumer(b).start();
+    }
+}
