c5d9cf35d77ce044bc87c4d8f1d7ab9cf975680c
Abhijit Mohanty <abhijitmohanty334@gmail.com>
2025-11-22 02:37:44 +0530

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,26 @@
+class FactorialThread extends Thread {
+    private int number;
+
+    public FactorialThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        long fact = 1;
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + number + " is " + fact);
+    }
+}
+
+public class FactorialMain {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 10};
+
+        for (int n : nums) {
+            new FactorialThread(n).start();
+        }
+    }
+}
