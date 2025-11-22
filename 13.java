28956b5a5d8eb4866a4fb2d92b7172f2b0f47b47
Abhijit Mohanty <abhijitmohanty334@gmail.com>
2025-11-22 12:13:55 +0530

    Update 13.java

--- 13.java
+++ 13.java
@@ -1,3 +1,7 @@
+//Write a Java program to create a vehicle class hierarchy. The base class should be Vehicle, with subclasses Truck, Car and Motorcycle. Each subclass should have properties such as make, model, year, and fuel type..Implement methods for
+calculating t fuel efficiency, (distance
+traveled, and maximum speed.
+
 class Vehicle {
     protected String make, model, fuelType;
     protected int year;
